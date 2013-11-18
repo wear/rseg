466cb5aa45441cf706b1a22ea088d89aaa4016ff
wear <wear63659220@gmail.com>
2013-11-18 16:12:35 +0800

    ignore dict generation file

--- spec/rseg_spec.rb
+++ spec/rseg_spec.rb
@@ -1,8 +1,11 @@
 # -*- encoding : utf-8 -*-
 require 'spec_helper'
+require 'builder/dict'
 
 describe Rseg do
   it '正常分词' do
+    dictFile = File.join(File.dirname(__FILE__), '../dict/dict.hash')
+    BuilderDict.build unless File.exist?(dictFile)
     Rseg.segment("需要分词的文章").should == ['需要', '分词', '的', '文章']
   end
 end
